462569e4c9dde3d1d0308275b639fc4d243bfd42
artrayme <artrayme@protonmail.com>
2023-03-30 00:44:30 +0300

    test: implement all tests

--- application/src/test/java/com/github/rsoi/controller/GreetingControllerTest.java
+++ application/src/test/java/com/github/rsoi/controller/GreetingControllerTest.java
@@ -27,13 +27,13 @@ class GreetingControllerTest {
     @Test
     void getGreetingById() throws Exception {
         mockMvc
-                // making a request "GET http://localhost:8080/greetings?greetingId=1"
+                // посылаем запрос вида "GET http://localhost:8080/greetings?greetingId=1"
                 .perform(get("/greetings").param("greetingId", "1"))
-                // print some additional information to console (not necessary)
+                // выводим информацию о запросе и ответе в консоль (можно не выводить, это сделано для красоты)
                 .andDo(print())
-                // expect HTTP status 200
+                // ожидаем HTTP код 200
                 .andExpect(status().isOk())
-                // and also expect "Hello world from Spring Service Bean!" in the response
+                // и также ожидаем сообщение "Hello world from Spring Service Bean!" в ответе
                 .andExpect(content().string(containsString("Hello world from Spring Service Bean!")));
     }
 
@@ -41,7 +41,7 @@ class GreetingControllerTest {
     void createGreeting() throws Exception {
         var testGreeting = new CreateGreetingRequest("Hi, my name is Test Greeting");
 
-        // send POST request with body
+        // отправляем POST запрос с объектом, который нужно создать
         MvcResult createdGreetingResponse =
                 mockMvc.perform(post("/greetings")
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -50,19 +50,35 @@ class GreetingControllerTest {
                         .andExpect(status().isOk())
                         .andReturn();
 
-        // parse response into java object
+        // контроллер возвращает json, который нужно превратить в java-объект
         GreetingResponse createdGreeting = new ObjectMapper()
                 .readerFor(GreetingResponse.class)
                 .readValue(createdGreetingResponse.getResponse().getContentAsString());
 
-        // check if created entity actually exists in DB
+        // Проверяем, что созданный объект действительно создался и мы можем его получить GET-запросом
         mockMvc.perform(get("/greetings").param("greetingId", String.valueOf(createdGreeting.getId())))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString(createdGreeting.getText())));
     }
 
-    // Util method. Just shows how to clean up code
+    @Test
+    void getNotFoundException() throws Exception {
+        mockMvc
+                // пытаемся получить несуществующую запись
+                .perform(get("/greetings").param("greetingId", "9999999"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+    @Test
+    void getInternalServerError() throws Exception {
+        mockMvc
+                // посылаем некорректный запрос
+                .perform(get("/greetings").param("greetingId", "a"))
+                .andDo(print())
+                .andExpect(status().isInternalServerError());
+    }
+    // вспомогательный метод (util method)
     public static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
--- application/src/test/java/com/github/rsoi/service/GreetingServiceImplTest.java
+++ application/src/test/java/com/github/rsoi/service/GreetingServiceImplTest.java
@@ -2,17 +2,18 @@ package com.github.rsoi.service;
 
 import com.github.rsoi.domain.Greeting;
 import com.github.rsoi.dto.CreateGreetingRequest;
+import com.github.rsoi.dto.GreetingResponse;
 import com.github.rsoi.repository.GreetingRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class GreetingServiceImplTest {
@@ -23,35 +24,76 @@ class GreetingServiceImplTest {
     @Mock
     private GreetingRepository greetingRepository;
 
-    private Greeting greeting;
-    @BeforeEach
-    public void setUp() {
-        greeting = new Greeting();
-        greeting.setId(42L);
-        greeting.setText("Service Test greeting");
-        Mockito.when(greetingRepository.getReferenceById(42L))
-                .thenReturn(greeting);
-    }
-
     @Test
     void getGreeting() {
-        assertEquals(greeting.getText(),
-                greetingService.getGreeting(greeting.getId()).getText());
+        // Given (Arrange)
+        // В этом блоке находится буквально всё то, что дано изначально.
+        // Создаём объекты, настраиваем объекты, добавляем записи в базу и т.д.
+        // В конкретно нашем случае создаём объект, который якобы лежит в репозитории (и базе)
+        // и настраиваем сам репозиторий (mock-репозиторий) так, чтобы он отдавал данный объект при запросе по id
+        Greeting expectedGreeting = new Greeting();
+        expectedGreeting.setId(123L);
+        expectedGreeting.setText("Service Test greeting");
+        when(greetingRepository.getReferenceById(expectedGreeting.getId()))
+                .thenReturn(expectedGreeting);
+
+        // When (Act)
+        // Вызов логики, которую тестируем.
+        // Spring подложил сервису mock-репозиторий вместо настоящего, что позволяет протестировать
+        // только логику сервиса, ведь все зависимости и входные данные мы контролируем сами.
+        var actualGreeting = greetingService.getGreeting(expectedGreeting.getId());
+
+        // Then (Assert)
+        // Проверяем результаты. Тестируемая логика может вернуть какой-то результат
+        // или изменить состояние объекта/базы. Здесь как раз сравниваем, соответствует
+        // ли результат ожидаемому.
+        // В нашем случае сравниваем то, что getGreeting вернул тот же текст приветствия,
+        // какой мы изначально сохранили в mock-репозиторий.
+        assertEquals(expectedGreeting.getId(), actualGreeting.getId());
+        assertEquals(expectedGreeting.getText(), actualGreeting.getText());
     }
 
     @Test
     void createGreeting() {
-        // given - precondition or setup
-//        given(greetingRepository.getReferenceById(greeting.getId()))
-//                .willReturn(null);
-//
-//        given(greetingRepository.save(greeting)).willReturn(greeting);
-//
-//        // when -  action or the behaviour that we are going test
-//        var greetingResponse = greetingService.createGreeting(new CreateGreetingRequest(greeting.getText()));
-//
-//        // then - verify the output
-//        assertThat(greetingResponse).isNotNull();
+        // Given (Arrange)
+        // Аналогично с предыдущим кодом, но здесь делаем mock не только на getReferenceById,
+        // но и на save (он будет вызван при вызове тестируемого метода сервиса)
+        Greeting greetingToSave = new Greeting();
+        greetingToSave.setId(42L);
+        greetingToSave.setText("Hi1");
+        when(greetingRepository.save(any(Greeting.class))).thenReturn(greetingToSave);
+        when(greetingRepository.getReferenceById(greetingToSave.getId())).thenReturn(greetingToSave);
 
+        // When (Act)
+        // Вызываем метод, который нужно протестировать. И только его! 
+        var request = new CreateGreetingRequest(greetingToSave.getText());
+        GreetingResponse actualGreeting = greetingService.createGreeting(request);
+
+        // Then (Assert)
+        // Сравниваем результат (actual) с ожидаемым (expected).
+        // Здесь мы используем getGreeting, но считаем, что он работает правильно,
+        // так как данный метод тестирует только createGreeting и ничего более.
+        var expectedGreeting = greetingService.getGreeting(actualGreeting.getId());
+        assertEquals(expectedGreeting.getId(), actualGreeting.getId());
+        assertEquals(expectedGreeting.getText(), actualGreeting.getText());
     }
+
+    @Test
+    void testRepositoryInit(){
+        // Given (Arrange)
+        var automaticallyGeneratedGreeting = new Greeting();
+        automaticallyGeneratedGreeting.setId(null);
+        automaticallyGeneratedGreeting.setText("Hello world from Spring Service Bean!");
+        when(greetingRepository.findAll())
+                .thenReturn(List.of());
+        greetingService = new GreetingServiceImpl(greetingRepository);
+
+        // When (Act)
+        greetingService.init();
+
+        // Then (Assert)
+        verify(greetingRepository, times(1)).findAll();
+        verify(greetingRepository, times(1)).save(automaticallyGeneratedGreeting);
+    }
+
 }
